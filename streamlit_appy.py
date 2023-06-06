4ecda6818729481043567ca2485480c020aaa203
spbsnowflake <135561562+spbsnowflake@users.noreply.github.com>
2023-06-06 13:57:47 -0400

    Update streamlit_appy.py
    add list to snowflake

--- streamlit_appy.py
+++ streamlit_appy.py
@@ -49,13 +49,17 @@ def get_fruit_load_list():
        return my_cur.fetchall()
     
 #add a button to load list
-if streamlit.button('Get Fruit Load List'):
+
+#Allow end user to add a fruit  to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return  'Thanks for adding '+ new_fruit
+ 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
+    back_from_functions = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
-#New Section to display fruityvice friuit addition
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
