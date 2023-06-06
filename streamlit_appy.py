ccd9b409aad5378d3777d52b93512b80b6e8eb17
spbsnowflake <135561562+spbsnowflake@users.noreply.github.com>
2023-06-06 13:19:04 -0400

    Update streamlit_appy.py
    add second text box

--- streamlit_appy.py
+++ streamlit_appy.py
@@ -40,3 +40,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
+
+#New Section to display fruityvice friuit addition
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
