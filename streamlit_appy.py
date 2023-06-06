80d1aa7f01b407821d863a831de8604379dcc7cd
spbsnowflake <135561562+spbsnowflake@users.noreply.github.com>
2023-06-06 13:48:26 -0400

    Update streamlit_appy.py
    snowflake data functions

--- streamlit_appy.py
+++ streamlit_appy.py
@@ -41,13 +41,18 @@ try:
 except URLERROR as e:
   streamlit.stop()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit list contains:")
-streamlit.dataframe(my_data_rows)
+streamlit.header("The fruit list contains:")
+#snowflake related functions
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor():
+       my_cur.execute("select * from fruit_load_list")
+        return  my_cur.fetchall()
+    
+#add a button to load list
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 #New Section to display fruityvice friuit addition
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
